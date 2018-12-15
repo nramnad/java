f19f6aae19318fae265a4ad53d80bc0c2622e656
nramnad <33083517+nramnad@users.noreply.github.com>
2018-12-15 11:36:39 +0530

    Delete hello.java

--- hello.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-   public static void main(String[] args) {
-      // Prints "Hello, I am here in the terminal window.
-      System.out.println("Hello, Our leaders");
-   }
-}
