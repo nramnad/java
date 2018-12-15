0049b93e4042c16a676088d99759df31b2a0d850
nramnad <33083517+nramnad@users.noreply.github.com>
2018-12-15 11:37:05 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, I am here in the terminal window.
+      System.out.println("Hello, Our leaders");
+   }
+}
