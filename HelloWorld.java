97f741af4d7df2ed5099db44ae352afaf64984f2
nramnad <33083517+nramnad@users.noreply.github.com>
2019-03-03 14:44:24 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("Hello all, Welcome Our cum Akhil Jose1 my Great Leaders");
+      System.out.println("Hello all, Welcome Our cum Akhil Jose my Great Leaders");
    }
 }
