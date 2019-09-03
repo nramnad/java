8c33b2ce5df1b1debc6deb28b2c81c869f960a4b
Ram <Ram@abc.com>
2019-09-03 18:56:42 +0530

    My Release 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("Hello all, Welcome to Naren, Vamshi and Karthik");
+      System.out.println("Hello all, Welcome to Pon, Murali etc..");
    }
 }
