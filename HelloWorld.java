a8359ab2a4e0fa0324528b376e3b08701ce0bd64
Ram <Ram@abc.com>
2019-10-12 14:10:32 +0530

    Eclispe Phonton 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("Hello all, Welcome to Deepak, Manoj, Vivek and Sembian");
+      System.out.println("Hello all, Welcome to Deepak, Divya, Sneha etc..");
    }
 }
