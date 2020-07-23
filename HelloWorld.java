b1546d15637c0f1a0e8c7afe4fd9949ca3be6b10
Vinodh <vinodhkaruppannan@gmail.com>
2020-07-23 09:13:00 +0530

    Good Morning

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("Hello all, Shahataaz and Yogesh");
+      System.out.println("Vinodh");
    }
 }
